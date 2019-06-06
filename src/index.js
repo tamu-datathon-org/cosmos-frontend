ab621916efded36a5091a9a71cd6414e5b978ab0
Josiah Coad <josiah@coad.net>
2019-06-05 19:44:19 -0700

    added theme

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import { BrowserRouter } from 'react-router-dom';
+
+const THEME = createMuiTheme({
+    palette: {
+        primary: {
+            dark: "#04bea8",
+            main: "#88cfc3",
+            light: "#cbe9e2"
+        },
+        secondary: {
+            dark: "#4286F3",
+            main: "#4FC2F8"
+        }
+    },
+});
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <MuiThemeProvider theme={THEME}>
+            <App />
+        </MuiThemeProvider>
     </BrowserRouter>,
     document.getElementById('root')
 );
