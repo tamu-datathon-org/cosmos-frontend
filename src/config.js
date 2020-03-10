38afdbde2b5eee7c86327638484130a7c3cabc24
Samarth Dave <samarthdave@users.noreply.github.com>
2020-03-09 23:38:57 -0500

    Import reactstrap & extract configuration dependencies

--- src/config.js
+++ src/config.js
@@ -1,4 +1,49 @@
-export default {
+const cyan = '#49c6b2';
+const blue = '#499cc6';
+const green = '#49c672';
+
+export const themeConfig = {
+    palette: {
+        //Palette Doc - https://material-ui.com/customization/palette/
+        //Color Doc - https://material-ui.com/customization/color/
+        //Color Picker Tool - https://material.io/inline-tools/color/
+        // Main provided, Dark, Light, and Contrast Text will be auto generated
+        primary: {
+            main: cyan,
+            contrastText: '#fff',
+        },
+        secondary: {
+            main: blue,
+        },
+        error: {
+            main: green,
+        },
+    },
+    typography: {
+        fontFamily: [
+            "'Comfortaa', 'cursive'",
+            "'Open Sans', 'sans-serif'",
+        ].join(','),
+    },
+    props: {
+        MuiTypography: {
+            variantMapping: {
+                h1: "'Comfortaa', 'cursive'",
+                h2: "'Open Sans', 'sans-serif'",
+                h3: 'h2',
+                h4: 'h2',
+                h5: 'h2',
+                h6: 'h2',
+                subtitle1: 'h2',
+                subtitle2: 'h2',
+                body1: 'span',
+                body2: 'span',
+            },
+        },
+    },
+};
+
+export const AWSConfig = {
     MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
         REGION: 'us-east-1',
--- src/index.js
+++ src/index.js
@@ -5,57 +5,18 @@ import registerServiceWorker from './registerServiceWorker';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { BrowserRouter } from 'react-router-dom';
 import Amplify from 'aws-amplify';
-import config from './config';
+// extract theme & credentials config
+import {
+    AWSConfig as config,
+    themeConfig
+} from './config';
 import './assets/scrollbar.css';
 
 //CssBaseline - https://material-ui.com/components/css-baseline/
 //Source Code - https://github.com/mui-org/material-ui/blob/master/packages/material-ui/src/CssBaseline/CssBaseline.js
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-const cyan = '#49c6b2';
-const blue = '#499cc6';
-const green = '#49c672';
-
-const THEME = createMuiTheme({
-    palette: {
-        //Palette Doc - https://material-ui.com/customization/palette/
-        //Color Doc - https://material-ui.com/customization/color/
-        //Color Picker Tool - https://material.io/inline-tools/color/
-        // Main provided, Dark, Light, and Contrast Text will be auto generated
-        primary: {
-            main: cyan,
-            contrastText: '#fff',
-        },
-        secondary: {
-            main: blue,
-        },
-        error: {
-            main: green,
-        },
-    },
-    typography: {
-        fontFamily: [
-            "'Comfortaa', 'cursive'",
-            "'Open Sans', 'sans-serif'",
-        ].join(','),
-    },
-    props: {
-        MuiTypography: {
-            variantMapping: {
-                h1: "'Comfortaa', 'cursive'",
-                h2: "'Open Sans', 'sans-serif'",
-                h3: 'h2',
-                h4: 'h2',
-                h5: 'h2',
-                h6: 'h2',
-                subtitle1: 'h2',
-                subtitle2: 'h2',
-                body1: 'span',
-                body2: 'span',
-            },
-        },
-    },
-});
+const THEME = createMuiTheme(themeConfig);
 
 Amplify.configure({
     Auth: {
